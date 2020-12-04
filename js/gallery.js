d0107ba716b21dcf1489e86d40cd730cc678c758
lowkey <saltyndea@gmail.com>
2020-12-04 15:24:25 -0700

    started infinite scroll

---
+++ js/gallery.js
@@ -0,0 +1,101 @@
+const imageContainer = document.querySelector('.gallery__container');
+const body = document.querySelector('body');
+
+let photosArray = [];
+
+// API
+const count = 3;
+const apiKey = 'dNz3c1X_j1AGBMw5Up-N-LmZtbJObnIxlu32YY22CUY';
+
+const randomInt = (min, max) => Math.floor(Math.random() * (max - min) + 1) + min;
+
+const apiUrl = `https://api.unsplash.com//photos/random/?client_id=${apiKey}&query=landscape&count=${count}`;
+
+
+
+const displayPhotos = function() {
+    // Run function for each object in array
+    photosArray.forEach((photo) => {
+        // Link on click to unsplash
+         
+        const item = document.createElement('a');
+        item.setAttribute('href', photo.links.html)
+        item.setAttribute('target', '_blank');
+
+        // Create img element for photo
+        const img = document.createElement('img');
+        img.setAttribute('src', photo.urls.regular);
+        img.setAttribute('alt', photo.alt_description);
+        img.setAttribute('title', photo.alt_description);
+        img.style.borderRadius = '2px'
+
+        // Add caption with photographer name
+        const figcapCont = document.createElement('div');
+        const figcap = document.createElement('figcaption');
+        figcap.innerHTML = `Photo by ${photo.user.name}`;
+
+         // Put image inside link & both inside img container element
+         figcapCont.appendChild(figcap);
+         item.appendChild(img);
+        imageContainer.appendChild(item);
+        imageContainer.appendChild(figcapCont);
+    });
+}
+
+// Get photos from unsplash API
+
+async function getPhotos() {
+    try{
+        const response = await fetch(apiUrl);
+        photosArray = await response.json();
+        
+        displayPhotos();
+
+        // Wait until there is a list of photos to make the intersecting element be the last img element otherwise on page load is null
+
+        // Intersection Observer for Infinite Scroll
+        let lastImg = imageContainer.lastElementChild.previousElementSibling.firstChild;
+
+        let offset = -lastImg + "px";
+
+        const options = {
+            root: null,
+            threshold: 0.7
+
+        }
+
+        // Careful to not limit the API request limit here
+        const obsCallback = function (entries, observer) {
+
+            //TODO Find out from the entries when to call getPhotos again (might be using rootMargin with offset in options instead of threshhold so it only goes one way?)
+
+            // Get more photos
+
+            // getPhotos();
+
+            // Unobserve old last img
+            lastImgObserver.unobserve(lastImg);
+
+            // Reset old las img to new last img
+            lastImg = imageContainer.lastElementChild.previousElementSibling.firstChild;
+
+            // Observe new lastImg
+            lastImgObserver.observe(lastImg);
+        }
+
+        const lastImgObserver = new IntersectionObserver(obsCallback, options);
+
+        lastImgObserver.observe(lastImg);
+        
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// On load
+getPhotos();
+
+
+
+
